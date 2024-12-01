06762df31ac2c3b2784ffe576d5d20bc76c8837e
Thiago Diegoli <thiago_diegoli@hotmail.com>
2024-12-01 18:54:01 -0300

    Atualizando models

--- server/models/ProductModel.js
+++ server/models/ProductModel.js
@@ -1,45 +1,44 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   tipo: {
     type: String,
-    enum: [
-      'material-de-consumo',
-      'material-permanente',
-      'hibrido',
-      'outro'
-    ],
-    required: true
+    required: true,
   },
   quantidade: { type: Number, required: true, min: 1 },
-  categoria: { 
-    type: String, 
+  categoria: {
+    type: String,
     enum: [
-      'material-de-escritorio',
-      'equipamentos-de-informatica',
-      'material-de-limpeza',
-      'materiais-didaticos-pedagogicos',
-      'material-de-escritorio-especializado',
-      'equipamentos-de-laboratorio',
-      'material-de-construcao-manutencao',
-      'moveis-equipamentos',
-      'material-de-higiene-saude',
-      'materiais-para-eventos-projetos-especiais',
-      'materiais-esportivos-educacao-fisica',
-      'recursos-tecnologicos-multimidia',
-      'material-de-arte-design',
-      'material-de-jardinagem-paisagismo',
-      'material-de-seguranca-prevencao',
-      'outro'
-    ], 
-    required: true 
+      "material-de-escritorio",
+      "equipamentos-de-informatica",
+      "material-de-limpeza",
+      "materiais-didaticos-pedagogicos",
+      "material-de-escritorio-especializado",
+      "equipamentos-de-laboratorio",
+      "material-de-construcao-manutencao",
+      "moveis-equipamentos",
+      "material-de-higiene-saude",
+      "materiais-para-eventos-projetos-especiais",
+      "materiais-esportivos-educacao-fisica",
+      "recursos-tecnologicos-multimidia",
+      "material-de-arte-design",
+      "material-de-jardinagem-paisagismo",
+      "material-de-seguranca-prevencao",
+      "outro",
+    ],
+    required: true,
   },
   descricao: { type: String },
   data: { type: Date, default: Date.now, required: true },
-  status: { type: String, enum: ['Pendente', 'Aprovado', 'Negado'], default: 'Pendente', required: true },
+  status: {
+    type: String,
+    enum: ["Pendente", "Aprovado", "Negado"],
+    default: "Pendente",
+    required: true,
+  },
   justificativa: { type: String },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 });
 
-export default mongoose.model('Product', productSchema);
+export default mongoose.model("Product", productSchema);
