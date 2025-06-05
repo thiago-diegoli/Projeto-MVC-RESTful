59867fd510c1698c6f7b3a223229fa2d6c57f724
Thiago Diegoli <81260348+thiago-diegoli@users.noreply.github.com>
2025-06-05 17:49:17 -0300

    Update ProductValidator.js

--- server/validators/ProductValidator.js
+++ server/validators/ProductValidator.js
@@ -1,38 +1,18 @@
 import { check } from 'express-validator';
 import Product from '../models/ProductModel.js';
 
-const allowedCategories = [
-  'material-de-escritorio',
-  'equipamentos-de-informatica',
-  'material-de-limpeza',
-  'materiais-didaticos-pedagogicos',
-  'material-de-escritorio-especializado',
-  'equipamentos-de-laboratorio',
-  'material-de-construcao-manutencao',
-  'moveis-equipamentos',
-  'material-de-higiene-saude',
-  'materiais-para-eventos-projetos-especiais',
-  'materiais-esportivos-educacao-fisica',
-  'recursos-tecnologicos-multimidia',
-  'material-de-arte-design',
-  'material-de-jardinagem-paisagismo',
-  'material-de-seguranca-prevencao',
-  'outro'
-];
-
 export const validaProduto = [
   check('nome')
     .notEmpty().withMessage('É obrigatório informar o nome'),
-
   check('tipo')
     .notEmpty().withMessage('O tipo é obrigatório'),
-
   check('quantidade')
     .notEmpty().withMessage('A quantidade é obrigatória')
     .isNumeric().withMessage('A quantidade deve ser um número')
     .isInt({ min: 1 }).withMessage('A quantidade deve ser maior ou igual a 1'),
-
   check('categoria')
     .notEmpty().withMessage('A categoria é obrigatória')
-    .isIn(allowedCategories).withMessage('A categoria informada é inválida'),
+    .isString().withMessage('A categoria deve ser um texto')
+    .trim()
+    .isLength({ min: 1 }).withMessage('A categoria não pode estar vazia'),
 ];
